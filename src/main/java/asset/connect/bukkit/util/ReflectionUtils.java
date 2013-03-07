23de4e7b77a832dc7775dd1f8b5772e0c1014642
Robert Coelho <robertcoelho@live.com>
2013-03-06 17:30:05 -0800

    Fix reflectionutils

--- src/main/java/asset/connect/bukkit/util/ReflectionUtils.java
+++ src/main/java/asset/connect/bukkit/util/ReflectionUtils.java
@@ -6,31 +6,22 @@ import java.lang.reflect.Modifier;
 
 public class ReflectionUtils {
 
-	public static boolean setFinalField(Class<?> objectClass, Object object, String fieldName, Object value) throws Exception {
-		try {
-			Field field = objectClass.getDeclaredField(fieldName);
-			field.setAccessible(true);
+	public static void setFinalField(Class<?> objectClass, Object object, String fieldName, Object value) throws Exception {
+		Field field = objectClass.getDeclaredField(fieldName);
+		field.setAccessible(true);
 
-			Field modifiersField = Field.class.getDeclaredField("modifiers");
-			modifiersField.setAccessible(true);
-			modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+		Field modifiersField = Field.class.getDeclaredField("modifiers");
+		modifiersField.setAccessible(true);
+		modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
 
-			field.set(object, value);
-			return true;
-		} catch (Exception exception) {
-			return false;
-		}
+		field.set(object, value);
 	}
 	
 	@SuppressWarnings("unchecked")
 	public static <T> T getPrivateField(Object object, Class<T> fieldClass, String fieldName) throws Exception {
-		try {
-			Field field = object.getClass().getDeclaredField(fieldName);
-			field.setAccessible(true);
-			return (T) field.get(object);
-		} catch(Exception exception) {
-			return null;
-		}
+		Field field = object.getClass().getDeclaredField(fieldName);
+		field.setAccessible(true);
+		return (T) field.get(object);
 	}
 	
 }
